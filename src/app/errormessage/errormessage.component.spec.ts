d689f18a5e9c2182bc9497aebe5d4fe739aab82c
GiovanaJensen <oliveirasgiovana143@gmail.com>
2024-07-07 18:03:10 -0300

    add: testes para verificar as mensagens de erro

--- src/app/errormessage/errormessage.component.spec.ts
+++ src/app/errormessage/errormessage.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing"
 import { ErrorMessageComponent } from "./errormessage.component"
+import { By } from "@angular/platform-browser";
 
 describe('ErrorMessageComponent', () => {
     let component: ErrorMessageComponent;
@@ -7,15 +8,39 @@ describe('ErrorMessageComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [ErrorMessageComponent],
-        }).compileComponents();
+            imports: [ErrorMessageComponent], // declaração do componente que será testado
+        }).compileComponents(); // compila o componente para que o TestBed possa utilza-lo
 
-        fixture = TestBed.createComponent(ErrorMessageComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+        fixture = TestBed.createComponent(ErrorMessageComponent); // cria a instância do componente
+        component = fixture.componentInstance; // pega a instância do componente para acessar propriedades e métodos
+        fixture.detectChanges(); // dispara a detecção de mudanças para atualizar o estado do componente
     })
 
     it('create component', () => {
-        expect(component).toBeTruthy();
+        expect(component).toBeTruthy(); // verifica se o componente foi criado com sucesso
+    });
+
+    it('renders default error state', () => {
+        const messageContainer = fixture.debugElement.query(
+            By.css('[data-testId="message-container"]')
+        );
+
+        expect(messageContainer.nativeElement.textContent).toBe(
+            'Something went wrong!'
+        );
+    });
+
+    it('renders custom error message', () => {
+        component.message = "Email is already taken!";
+
+        fixture.detectChanges();
+
+        const messageContainer = fixture.debugElement.query(
+            By.css('[data-testId="message-container"')
+        );
+
+        expect(messageContainer.nativeElement.textContent).toBe(
+            'Email is already taken!'
+        );
     });
 });
