54dd9c8b5cfbf95419f91050dbedb7111b98b578
Giovana Fernandes <oliveirasgiovana143@gmail.com>
2025-01-02 16:55:32 -0300

    realizando testes unitários para somar, subtrair, dividir e multiplicar

--- src/app/_services/calculadora.service.spec.ts
+++ src/app/_services/calculadora.service.spec.ts
@@ -12,7 +12,27 @@ describe('CalculadoraService', () => {
     service = TestBed.inject(CalculadoraService);
   });
 
-  it('should be created', () => {
+  it('Deve somar, corretamente, dois números', () => {
     expect(service).toBeTruthy();
+    const result = service.calcular(5,8,'soma');
+    expect(result).toBe(13, "O resultado deve ser igual a 13")
   });
+
+  it('Deve subtrair, corretamente, dois números', () => {
+    expect(service).toBeTruthy();
+    const result = service.calcular(16,3,'subtração');
+    expect(result).toBe(13, 'O resultado deve ser igual a 13')
+  })
+
+  it("Deve dividir, corretamente, dois números", () => {
+    expect(service).toBeTruthy();
+    const result = service.calcular(6,2, 'divisão');
+    expect(result).toBe(3, "O resultado deve ser igual a 3")
+  })
+
+  it("Deve multiplicar, corretamente, dois números", () => {
+    expect(service).toBeTruthy();
+    const result = service.calcular(2,2, 'multiplicação');
+    expect(result).toBe(4, "O resultado deve ser igual a 4")
+  })
 });
